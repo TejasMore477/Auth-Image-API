f832b37052eeea558d37af31d78e44fa47cbe29b
Tejas More <tejasmore660@gmail.com>
2025-03-31 18:39:51 +0530

    created cloudinary-heaper file to add files to cloudinary and get the result object with url and publicId

---
+++ helpers/cloudinary-helper.js
@@ -0,0 +1,21 @@
+const cloudinaryConfigs = require('../config/cloudinary.js');
+
+const uploadToCloudinary = async (filepath) => {
+    try {
+        const result = await cloudinaryConfigs.uploader.upload(filepath)
+
+        return {
+            url : result.secure_url,
+            publicId : result.public_id
+        };
+        
+    } catch (error) {
+        console.log('Error occured while uploading data to Cloudinary \n', error);
+        throw new Error('Error occured while uploading data to Cloudinary')
+        
+    }
+};
+
+module.exports = {
+    uploadToCloudinary
+}
